9b1e7def4e45be1995ae2c3ddf584401b41dd1db
unknown <pedrovalente_00@hotmail.com>
2023-04-02 12:48:18 +0200

    added routing functionality with react router dom version 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 import Navbar from './Navbar';
 import Home from "./Home";
+import Create from './Create';
+
 function App() {
     return (
+        <BrowserRouter>
         <div className="App">
             <Navbar/>
             <div className="content">
-                <Home/>
+                <Switch>
+                    <Route exact path="/"><Home/></Route>
+                    <Route exact path="/create"><Create/></Route>
+                </Switch>
             </div>
         </div>
+        </BrowserRouter>
     );
 }
 
---
+++ src/Create.tsx
@@ -0,0 +1,7 @@
+const Create = () => {
+    return ( <section className="create">
+        <h2>Add a new blog</h2>
+    </section> );
+}
+ 
+export default Create;
